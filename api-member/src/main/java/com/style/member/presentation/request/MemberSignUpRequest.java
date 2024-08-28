226430626fba4d8839b6b785eaca674a6164b483
Greg.Study <greg0917@naver.com>
2024-08-28 19:22:24 +0900

    refactor: 네이밍 변경

--- api-member/src/main/java/com/style/member/presentation/request/MemberSignUpRequest.java
+++ api-member/src/main/java/com/style/member/presentation/request/MemberSignUpRequest.java
@@ -15,16 +15,16 @@ import static com.style.member.utils.MemberConst.*;
 public class MemberSignUpRequest {
 
     @NotBlank(message = "이메일은 필수 입력 입니다.")
-    @Pattern(regexp = USER_EMAIL_REGEXP, message = "이메일 형식이 올바르지 않습니다.")
+    @Pattern(regexp = MEMBER_EMAIL_REGEXP, message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
     @NotBlank(message = "닉네임은 필수 입력 입니다.")
     @Size(max = 10, message = "닉네임은 10글자 이하이어야 합니다.")
-    @Pattern(regexp = USER_NICKNAME_REGEXP, message = "닉네임은 한글 또는 영어이어야 합니다.")
+    @Pattern(regexp = MEMBER_NICKNAME_REGEXP, message = "닉네임은 한글 또는 영어이어야 합니다.")
     private String nickname;
 
     @NotBlank(message = "비밀번호는 필수 입력 입니다.")
-    @Pattern(regexp = USER_PASSWORD_REGEXP, message = "비밀번호는 10~16자 영문 대 소문자, 숫자, 특수문자 형식이어야 합니다.")
+    @Pattern(regexp = MEMBER_PASSWORD_REGEXP, message = "비밀번호는 10~16자 영문 대 소문자, 숫자, 특수문자 형식이어야 합니다.")
     private String password;
 
     private Boolean isAdmin;
--- api-member/src/main/java/com/style/member/utils/MemberConst.java
+++ api-member/src/main/java/com/style/member/utils/MemberConst.java
@@ -5,8 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class MemberConst {
 
-    public static final String USER_EMAIL_REGEXP = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$";
-    public static final String USER_NICKNAME_REGEXP = "^[가-힣a-zA-Z]+$";
-    public static final String USER_PASSWORD_REGEXP = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{10,16}";
+    public static final String MEMBER_EMAIL_REGEXP = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$";
+    public static final String MEMBER_NICKNAME_REGEXP = "^[가-힣a-zA-Z]+$";
+    public static final String MEMBER_PASSWORD_REGEXP = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{10,16}";
 
 }
